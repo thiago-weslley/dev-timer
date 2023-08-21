43596d6bdf5efb96c5b09de6b6a5ef10227b49af
thiago-weslley <thiagoweslley@yahoo.com>
2023-08-21 17:11:11 -0300

    feat(router): Adiciona navegação com `react-router-dom`
    - Criados os arquivos "History.tsx" e "Home.tsx" para as páginas History e Home, respectivamente.
    
    - Criado o arquivo "Router.tsx" para configurar as rotas usando "Routes" e "Route" do "react-router-dom".
    
    - Atualizado o arquivo "App.tsx" para envolver o roteador em um "BrowserRouter".

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./styles/globalStyles";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { Router } from "./routes/Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Em construção...</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
