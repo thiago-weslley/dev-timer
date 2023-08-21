8fc1c7244e47274f5171b5c406d14e41d5155072
thiago-weslley <thiagoweslley@yahoo.com>
2023-08-21 15:23:54 -0300

    feat(styles): Integra tema personalizado e estilos globais
    Atualizei o "App.ts" para usar o "ThemeProvider" com o "defaultTheme".
    
    Adicionei estilos globais no "globalStyles.ts" para ajustar fontes, cores e foco.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { GlobalStyle } from "./styles/globalStyles";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Em construção...</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
