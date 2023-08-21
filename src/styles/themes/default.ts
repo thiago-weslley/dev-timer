747bafaed1ee57309ff200bcd261bb48671f5779
thiago-weslley <thiagoweslley@yahoo.com>
2023-08-21 20:42:28 -0300

    fix(color): Corrige cor do tema `gray-800`

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -6,7 +6,7 @@ export const defaultTheme = {
   "gray-500": "#7C7C8A",
   "gray-600": "#323238",
   "gray-700": "#29292E",
-  "gray-800": "#202824",
+  "gray-800": "#202024",
   "gray-900": "#121214",
   "green-300": "#00B37E",
   "green-500": "#00875F",
