98cd2cf42cdbb7a19b7683fa103d44d0e621ce8c
thiago-weslley <thiagoweslley@yahoo.com>
2023-08-22 15:35:24 -0300

    fix: Corrige height de `DefaultLayout`

--- src/pages/layouts/DefaultLayout/styles.ts
+++ src/pages/layouts/DefaultLayout/styles.ts
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
   max-width: 90rem;
+  min-height: calc(100vh - 17rem);
   display: flex;
   flex-direction: column;
   margin: 0 auto;
-  margin-top: 10rem;
+  margin-top: 6rem;
   padding: 3.5rem;
   border-radius: 10px;
   background: ${(props) => props.theme["gray-800"]};
