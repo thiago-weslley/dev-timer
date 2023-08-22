ff238324957320b8e1e5f05ff550a0e467e30478
thiago-weslley <thiagoweslley@yahoo.com>
2023-08-22 14:25:17 -0300

    style: Aplica `box-shadow` e `text-shadow`

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -64,12 +64,15 @@ export const TimerContainer = styled.div`
     background: ${(props) => props.theme["gray-700"]};
     padding: 0rem 1.5rem;
     border-radius: 8px;
+    text-shadow: 3px 3px 8px ${(props) => props.theme.black};
+    box-shadow: 2px 2px 15px ${(props) => props.theme.black};
   }
 `;
 
 export const Separator = styled.div`
   overflow: hidden;
   color: ${(props) => props.theme["green-500"]};
+  text-shadow: 3px 3px 10px ${(props) => props.theme.black};
 `;
 
 export const StartTimer = styled.button`
@@ -85,6 +88,7 @@ export const StartTimer = styled.button`
   font-weight: bold;
   color: ${(props) => props.theme["gray-100"]};
   background: ${(props) => props.theme["green-500"]};
+  box-shadow: 2px 2px 15px ${(props) => props.theme.black};
   cursor: pointer;
   transition: background 0.1s ease-in-out;
 
--- src/pages/layouts/DefaultLayout/styles.ts
+++ src/pages/layouts/DefaultLayout/styles.ts
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
-  max-width: 110rem;
+  max-width: 90rem;
   display: flex;
   flex-direction: column;
-  margin: 8rem auto 0 auto;
+  margin: 0 auto;
+  margin-top: 10rem;
   padding: 3.5rem;
   border-radius: 10px;
   background: ${(props) => props.theme["gray-800"]};
+  box-shadow: 2px 2px 10px ${(props) => props.theme.black};
 `;
