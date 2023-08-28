cb4555ca1cd983f42d0a5d5fc4d23cd779515836
thiago-weslley <thiagoweslley@yahoo.com>
2023-08-28 16:17:00 -0300

    chore(date): Formata campo `início` da `History`

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 import { CyclesContext } from "../../contexts/CyclesContext";
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
 export function History() {
   const { cycles } = useContext(CyclesContext);
@@ -22,8 +24,13 @@ export function History() {
             {cycles.map((cycle) => (
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
-                <td>{cycle.minutesAmount} Min</td>
-                <td>{cycle.startDate.toISOString()}</td>
+                <td>{cycle.minutesAmount} Minutos</td>
+                <td>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
                 <td>
                   {cycle.finishedDate && (
                     <Status statusColor="green">Concluído</Status>
